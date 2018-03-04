75d5c7de31814ac33d3e3562a9e0d8e7426a34c4
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-04 10:25:01 +0100

    Going to settings and applying them

--- src/views/NewNoteView.js
+++ src/views/NewNoteView.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react';
 import Link from 'react-router-dom/Link';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
 import { addNote } from '../actions';
 import { bindActionCreators } from 'redux';
 
+let self ;
 class NewNoteView extends Component{
 
     constructor(){
         super();
-        this.state = {note:''};
+        this.state = {note:'', isEnterPressed:false};
+        self = this;
     }
 
     handleChange(e){
         this.setState({note:e.target.value});
+    }
+
+    handleKeyPress(e){
+        if(e.charCode == '13'){
+            self.validateNote();
+            self.setState({isEnterPressed:true});
+        }
     }
 
     validateNote(){
@@ -23,12 +33,19 @@ class NewNoteView extends Component{
     }
 
     render(){
+        if(this.state.isEnterPressed){
+            return <Redirect push to={`/board/${this.state.login}`}></Redirect>
+        }
+
         return(
             <div>
             <h3>New note for {this.props.match.params.user}</h3>
-            <input  placeholder="Your note" onChange = {(e)=>this.handleChange(e)}/>
+            <input  placeholder="Your note"
+             onChange = {(e)=>this.handleChange(e)}
+             onKeyPress={this.handleKeyPress}/>
+             <br/>
             <Link to={`/board/${this.props.match.params.user}`} >
-            <br/>
+            
                 <button onClick={(e) => {this.validateNote()}}>Add this now !</button>
             </Link>
             </div>
--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Link from 'react-router-dom/Link';
 
 class SettingsView extends Component{
     render(){
@@ -6,6 +7,9 @@ class SettingsView extends Component{
             <div>
                 <h3>Settings view</h3>
                 <p>The settings</p>
+                <Link to={`/board/${this.props.match.params.user}`} >
+                    <button>Apply</button>
+                </Link>
             </div>
         );
     }
