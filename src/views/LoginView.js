aa42dda447d3916355a929e939dc1ad2b948e9af
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-04 07:16:45 +0100

    Enble/disable login button

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 
 class LoginView extends Component{
 
+    constructor(){
+        super();
+        this.state = {login:''}
+    }
+
+    handleChange(e){
+        this.setState({login: e.target.value});
+    }
+
 render(){
     return(
         <div>
             <h3>Login view</h3>
-            <input placeholder="type your login" />
+            <input placeholder="type your login" onChange={(e) => {this.handleChange(e)}} ref= {(input)=> this.logInput = input}/>
             <br/>
-            <button>Go on !</button>
+            <button disabled = {!this.state.login}>Go on !</button>
         </div>
     );
 }
