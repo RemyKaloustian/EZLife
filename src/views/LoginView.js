caec5a6d5d8ef4089a140e349fd83af7e985b86c
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-04 10:09:39 +0100

    Login to bard w/ Enter input key

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
+import { Redirect } from 'react-router';
 
-
+let self;
 class LoginView extends Component{
 
+    
     constructor(){
         super();
-        this.state = {login:''}
+        this.state = {login:'', isEnterPressed:false}
+        self = this;
     }
 
     handleChange(e){
         this.setState({login: e.target.value});
+    }   
+
+    handleKeyPress(e){        
+        if(e.charCode == 13){            
+            self.setState({isEnterPressed:true});
+        }
     }
 
-render(){
-    return(
-        <div>
-            <h3>Login view</h3>
-            <input placeholder='type your login' onChange={(e) => {this.handleChange(e)}} ref= {(input)=> this.logInput = input}/>
-            <br/>
-            <Link to={`/board/${this.state.login}`}>
-                <button disabled = {!this.state.login}>Go on !</button>
-            </Link>
-        </div>
-    );
-}
+    render(){
+        if(this.state.isEnterPressed == true){         
+            return <Redirect push to={`/board/${this.state.login}`}></Redirect>
+        }
 
+        return(
+            <div>
+                <h3>Login view</h3>
+                <input placeholder='type your login' 
+                    onChange={(e) => {this.handleChange(e)}} ref= {(input)=> this.logInput = input}
+                    onKeyPress={this.handleKeyPress}/>
+                <br/>
+                <Link to={`/board/${this.state.login}`}>
+                    <button disabled = {!this.state.login}>Go on !</button>
+                </Link>
+            </div>
+        );
+    }
 }
 
 export default LoginView;
