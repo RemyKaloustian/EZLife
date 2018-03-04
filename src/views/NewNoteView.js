c9658121c2465c061306032dda81e57b773caa22
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-04 08:50:01 +0100

    Adding input note to store

--- src/views/NewNoteView.js
+++ src/views/NewNoteView.js
@@ -6,14 +6,30 @@ import { addNote } from '../actions';
 import { bindActionCreators } from 'redux';
 
 class NewNoteView extends Component{
+
+    constructor(){
+        super();
+        this.state = {note:''};
+    }
+
+    handleChange(e){
+        this.setState({note:e.target.value});
+    }
+
+    validateNote(){
+        if(this.state.note.trim().length > 0){
+            this.props.dispatch(addNote(this.state.note));
+        }
+    }
+
     render(){
         return(
             <div>
             <h3>New note for {this.props.match.params.user}</h3>
-            <input  placeholder="Your note"/>
+            <input  placeholder="Your note" onChange = {(e)=>this.handleChange(e)}/>
             <Link to={`/board/${this.props.match.params.user}`} >
             <br/>
-                <button onClick={(e) => {this.props.dispatch(addNote('héhé'))}}>Add this now !</button>
+                <button onClick={(e) => {this.validateNote()}}>Add this now !</button>
             </Link>
             </div>
         );
