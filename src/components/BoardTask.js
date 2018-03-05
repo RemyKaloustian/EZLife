57c6106fe5f2be8ff7433371f2019c1b00f517f7
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-05 16:45:55 +0100

    style

--- src/components/BoardTask.js
+++ src/components/BoardTask.js
@@ -21,11 +21,11 @@ class BoardTask extends Component {
         return ( 
             <div>
                 <Link to={`/details/${this.props.user}/${this.props.item.name}`}>
-                                <h4 key={this.props.index}>{this.props.item.name}</h4>
-                                { 
-                                    this.props.item.subtasks.map((sub, subindex)=>
-                                    <p key={subindex}>{sub}</p>)
-                                }
+                    <h4 key={this.props.index}>{this.props.item.name}</h4>
+                    { 
+                        this.props.item.subtasks.map((sub, subindex)=>
+                        <p key={subindex}>{sub}</p>)
+                    }
                 </Link>
                 <button onClick={()=> this.deleteNote()}>Delete</button>
             </div>
