a2625fbe67dbc28ab95ce929dcec86850c8e9138
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-08 07:37:40 +0100

    Toggle done/undone

--- src/components/SubTask.js
+++ src/components/SubTask.js
@@ -3,11 +3,34 @@ import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 
 import {toggleSubTask} from '../actions';
+
+const TOGGLE_BUTTON_DONE = 'Done';
+const TOGGLE_BUTTON_UNDONE = 'Undone';
+
+
 class SubTask extends Component {
+
+    constructor(props) {
+        super(props);
+        let isDone = this.props.notes[this.props.index].subtasks[this.props.subindex].done
+        if(isDone){
+            this.state = {toggleButton:TOGGLE_BUTTON_UNDONE};
+        }
+        else{
+            this.state = {toggleButton:TOGGLE_BUTTON_DONE};            
+        }
+    }
+    
 
     doSub(){
         let isDone = !this.props.notes[this.props.index].subtasks[this.props.subindex].done
         this.props.dispatch(toggleSubTask(this.props.index, this.props.name, isDone));
+        if(isDone){
+            this.setState({toggleButton:TOGGLE_BUTTON_UNDONE});
+        }
+        else{
+            this.setState({toggleButton:TOGGLE_BUTTON_DONE});        
+        }
     }
 
     render() { 
@@ -16,7 +39,7 @@ class SubTask extends Component {
             <div>
                 <p>{this.props.name}</p>
                 <p>{`${this.props.notes[this.props.index].subtasks[this.props.subindex].done}`}</p>
-                <button onClick={()=> this.doSub()}>Done</button>
+                <button onClick={()=> this.doSub()}>{this.state.toggleButton}</button>
             </div>
 
          )
