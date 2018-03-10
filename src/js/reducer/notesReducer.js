c90068f0c71a8e54fc4ae5344224798e384b2450
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 16:03:36 +0100

    Commenting notes reducer

--- src/js/reducer/notesReducer.js
+++ src/js/reducer/notesReducer.js
@@ -1,5 +1,7 @@
+// File imports
 import * as actionType from '../actions/ActionType';
 
+//Functions used in data creation
 const createNote = (id, name, subtasks)=>{
     return {id,  name, subtasks};
 }
@@ -8,24 +10,25 @@ const createSubtask = (name, done) =>{
     return {name, done}
 }
 
+//Our "database"
 const initialNotes = [
     createNote(0, 'Be awesome', [createSubtask('awesome1',false), createSubtask('awesome2',true)]),
     createNote(1, 'Be phenomenal', [createSubtask('phenomenal1',false), createSubtask('phenomenal2',false)]),
     createNote(2, 'Be the greatest', [createSubtask('greatest1',false), createSubtask('greatest2',false)]),
 ];
 
+// The notes reducer, gathering all the operations 
+//Siwthing on the action type received, and then proceeding accordingly
 
 const notesReducer = (state = initialNotes, action) => {
     switch (action.type) {
         case actionType.ADD_NOTE:   
-            console.log("ADD_NOTE, w/ action.payload = "+ action.payload);     
             return [...state, createNote(state.length, action.payload, [])];
 
         case actionType.DELETE_NOTE:
             return [...state.slice(0,action.payload), ...state.slice(action.payload+1, state.length)];
         
         case actionType.ADD_SUBTASK:
-            console.log(action.payload.note);
             const i = action.payload.note;
             return [...state.slice(0,i), {...state[i], subtasks:[...state[i].subtasks,createSubtask(action.payload.subtask, false)]},...state.slice(i+1, state.length)];
         
