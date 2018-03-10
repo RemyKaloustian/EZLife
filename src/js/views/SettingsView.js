5f39096276b90df30811d289bd33909e80665f80
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 17:02:22 +0100

    Commenting settings view

--- src/js/views/SettingsView.js
+++ src/js/views/SettingsView.js
@@ -1,15 +1,17 @@
+// Module imports
 import React, {Component} from 'react';
 import Link from 'react-router-dom/Link';
 
+// File imports
 import {getRandomColor} from '../utils/colors';
 
 class SettingsView extends Component{
     render(){
         return (
             <div>
-            <div className='header-panel' style={{backgroundColor: getRandomColor()}}>
-                <h3 className='header-title'>Settings view</h3>
-            </div>
+                <div className='header-panel' style={{backgroundColor: getRandomColor()}}>
+                    <h3 className='header-title'>Settings view</h3>
+                </div>
                 <div className='page-content alternative-content'>
                     <p>The settings</p>
                     <Link to={`/board/${this.props.match.params.user}`} >
