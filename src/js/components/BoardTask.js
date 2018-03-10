4354dcecb234cc527006e13984e3be41dfcbd3ba
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 15:54:39 +0100

    Commenting BoardTask

--- src/js/components/BoardTask.js
+++ src/js/components/BoardTask.js
@@ -1,22 +1,25 @@
+// Module imports
 import React, {Component} from 'react';
 import Link from 'react-router-dom/Link';
-
-
-import { deleteNote } from '../actions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+// File imports
+import { deleteNote } from '../actions';
 import BoardSubTask from './BoardSubTask';
 import {getRandomColor, getRandomColorSecondary} from '../utils/colors';
+
+//A task diplayed on Board View
 class BoardTask extends Component {
 
     constructor(props) {
         super(props);
         this.state = {id: this.props.item.id};
     }
+
     deleteNote (){
         this.props.dispatch(deleteNote(this.state.id));
-    }
-    
+    }    
     
     render() { 
         return ( 
@@ -25,7 +28,7 @@ class BoardTask extends Component {
                     <h4 key={this.props.index} className='gotham-light board-title'>{this.props.item.name}</h4>
                     { 
                         this.props.item.subtasks.map((sub, subindex)=>
-                        <BoardSubTask key={subindex} name={sub.name} done = {sub.done} className='gotham-thin board-subtask'></BoardSubTask>)
+                        <BoardSubTask key={subindex} name={sub.name} done = {sub.done} className='gotham-thin board-subtask'/>)
                     }
                 </Link>
                 <button className='delete-task-btn' style={{backgroundColor: getRandomColorSecondary()}} onClick={()=> this.deleteNote()}><img src='src/assets/icons/delete.png'/></button>
