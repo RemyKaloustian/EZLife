66ddefe3af3b87eba0190607c50e160f4cef1302
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 16:57:44 +0100

    Commenting login view

--- src/js/views/LoginView.js
+++ src/js/views/LoginView.js
@@ -1,10 +1,11 @@
+// Module imports
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
 import { Redirect } from 'react-router';
 
-let self;
+let self; // Is used so we can have access to this.setState in function with events params 
+// The login view, home page
 class LoginView extends Component{
-
     
     constructor(){
         super();
@@ -23,16 +24,16 @@ class LoginView extends Component{
     }
 
     render(){
+        //If we pressed enter, redirecting to the board
         if(this.state.isEnterPressed == true){         
             return <Redirect push to={`/board/${this.state.login}`}></Redirect>
         }
-
         return(
             <div>
                 <h3>Login view</h3>
                 <input placeholder='type your login' 
-                    onChange={(e) => {this.handleChange(e)}} ref= {(input)=> this.logInput = input}
-                    onKeyPress={this.handleKeyPress}/>
+                        onChange={(e) => {this.handleChange(e)}} ref= {(input)=> this.logInput = input}
+                        onKeyPress={this.handleKeyPress}/>
                 <br/>
                 <Link to={`/board/${this.state.login}`}>
                     <button disabled = {!this.state.login}>Go on !</button>
