c434c17e6088eb66e5551c655d65e3f9655b2626
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 15:46:31 +0100

    Commenting actions index.js

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,19 +1,8 @@
+//File imports
 import * as actionType from './ActionType';
 
-export const addCounter = () => ({
-  type: actionType.ADD_COUNTER,
-  payload: 1
-});
-
-export const removeCounter = () => ({
-  type: actionType.REMOVE_COUNTER,
-  payload: 1
-});
-
-export const displayList = ()=>({
-  type: actionType.DISPLAY_LIST,
-  payload:'haha'
-});
+//All the functions for action types used in notesReducer.js
+//Every function is called by the component that needs it
 
 export const addNote = (note) =>({
   type: actionType.ADD_NOTE,
