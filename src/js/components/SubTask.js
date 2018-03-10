79d7c6874f9bc38a3263874d79f9c9ae1eaf806a
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-03-10 15:58:29 +0100

    Commenting SubTask

--- src/js/components/SubTask.js
+++ src/js/components/SubTask.js
@@ -1,18 +1,22 @@
+// Module imports
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 
+// File imports
 import {toggleSubTask} from '../actions';
 
 const TOGGLE_BUTTON_DONE = 'Done';
 const TOGGLE_BUTTON_UNDONE = 'Undone';
-
 const TASK_DONE_CLASS = 'task-done';
 const TASK_UNDONE_CLASS='task-undone';
 
+// A subtask displayed in Details View
 class SubTask extends Component {
+
     constructor(props) {
         super(props);
+        //Checking at the beginning if the task is done or undone
         let isDone = this.props.notes[this.props.index].subtasks[this.props.subindex].done
         if(isDone){
             this.state = {toggleButton:TOGGLE_BUTTON_UNDONE, classN:TASK_DONE_CLASS};
@@ -25,16 +29,17 @@ class SubTask extends Component {
     doSub(){
         let isDone = !this.props.notes[this.props.index].subtasks[this.props.subindex].done
         this.props.dispatch(toggleSubTask(this.props.index, this.props.name, isDone));
+        //If task done, we undo it
         if(isDone){
             this.setState({toggleButton:TOGGLE_BUTTON_UNDONE, classN:TASK_DONE_CLASS});
         }
+        //If task undone, we do it and place a line through it
         else{
             this.setState({toggleButton:TOGGLE_BUTTON_DONE, classN:TASK_UNDONE_CLASS});        
         }
     }
 
     render() { 
-
         return ( 
             <div className='details-subtask' onClick={()=> this.doSub()}>
                 <p className={`${this.state.classN}`} >{this.props.name} </p>
@@ -48,7 +53,6 @@ function mapStateToProps(state){
         notes: state.notesReducer,
       };
 }
-
 
 function mapDispatchToProps(dispatch){
     return {actions: bindActionCreators(toggleSubTask, dispatch)}
